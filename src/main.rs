cff6682df762a4a2143edf0db19359674579793a
tlrobrn <obrien.taylorm@gmail.com>
2015-05-29 10:23:18 -0500

    Change argument handling to work with Rust 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-#![feature(core)]
 mod maze;
 use maze::Maze;
 use std::env;
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    let (width, height) = match args.as_slice() {
-        [_, ref width, ref height] => (width.parse(), height.parse()),
-        _ => (Ok(5), Ok(5)),
+    let args: Vec<String> = env::args().collect();
+    let (width, height) = if args.len() == 3 {
+        (args[1].parse(), args[2].parse())
+    } else {
+        (Ok(5), Ok(5))
     };
 
     match (width, height) {
